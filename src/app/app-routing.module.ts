ebb556a3d02e290adefd796b692e5dce616e47c9
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-04-04 18:27:54 -0300

    Actualizand App Routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ColaboracionesComponent } from './components/colaboraciones/colaboraciones.component';
+import { CuerpoComponent } from './components/cuerpo/cuerpo.component';
+import { DestacadosComponent } from './components/destacados/destacados.component';
+import { LuciblogComponent } from './components/luciblog/luciblog.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NovedadesComponent } from './components/novedades/novedades.component';
+import { PublicacionComponent } from './components/publicacion/publicacion.component';
+import { ReflexionesComponent } from './components/reflexiones/reflexiones.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/luciBlog', pathMatch: 'full' },
+  { path: 'luciBlog', component: LuciblogComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'cuerpo', component: CuerpoComponent },
+  { path: 'destacados', component: DestacadosComponent },
+  { path: 'novedades', component: NovedadesComponent },
+  { path: 'publicacion', component: PublicacionComponent },
+  { path: 'reflexiones', component: ReflexionesComponent },
+  { path: 'colaboraciones', component: ColaboracionesComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
