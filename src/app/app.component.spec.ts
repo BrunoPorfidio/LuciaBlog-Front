5cae36c33898b8a6e6bc55a87cc9c192c802d643
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-04-04 18:27:40 -0300

    Actualizando App Component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'Blog-Front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Blog-Front');
   });
 
   it('should render title', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PublicacionComponent } from './components/publicacion/publicacion.component';
+import { NovedadesComponent } from './components/novedades/novedades.component';
+import { LuciblogComponent } from './components/luciblog/luciblog.component';
+import { DestacadosComponent } from './components/destacados/destacados.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReflexionesComponent } from './components/reflexiones/reflexiones.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ColaboracionesComponent } from './components/colaboraciones/colaboraciones.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PublicacionComponent,
+    NovedadesComponent,
+    LuciblogComponent,
+    DestacadosComponent,
+    FooterComponent,
+    ReflexionesComponent,
+    SobreMiComponent,
+    ColaboracionesComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
