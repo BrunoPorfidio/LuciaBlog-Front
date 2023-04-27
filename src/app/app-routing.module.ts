fd9c2dc31fb8618bc358ec7a440e9e4b92b3dee7
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-04-27 02:26:34 -0300

    Agregnado nuevas rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,10 @@ import { LuciblogComponent } from './components/luciblog/luciblog.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NovedadesComponent } from './components/novedades/novedades.component';
 import { PublicacionComponent } from './components/publicacion/publicacion.component';
+import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
 import { ReflexionesComponent } from './components/reflexiones/reflexiones.component';
+import { ModalEditarPublicacionComponent } from './modals/modal-editar-publicacion/modal-editar-publicacion.component';
+import { ModalNuevoPublicacionComponent } from './modals/modal-nuevo-publicacion/modal-nuevo-publicacion.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/luciBlog', pathMatch: 'full' },
@@ -15,6 +18,9 @@ const routes: Routes = [
   { path: 'destacados', component: DestacadosComponent },
   { path: 'novedades', component: NovedadesComponent },
   { path: 'publicacion', component: PublicacionComponent },
+  { path: 'publicaciones', component: PublicacionesComponent },
+  { path: 'nuevo-publicacion', component: ModalNuevoPublicacionComponent },
+  { path: 'editar-publicacion/:idPublicacion', component: ModalEditarPublicacionComponent },
   { path: 'reflexiones', component: ReflexionesComponent },
   { path: 'colaboraciones', component: ColaboracionesComponent  }
 ];
