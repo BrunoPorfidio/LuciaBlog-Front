bcb66957eed4f13506395361ec2308e5f0e58db9
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-05-24 20:33:15 -0300

    Agregando Modal Editar para Publicacion.

--- src/app/modals/modal-editar-publicacion/modal-editar-publicacion.component.ts
+++ src/app/modals/modal-editar-publicacion/modal-editar-publicacion.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Persona } from 'src/app/Models/Persona';
-import { PersonaService } from 'src/app/Services/persona.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Publicacion } from 'src/app/Models/Publicacion';
+import { PublicacionService } from 'src/app/Services/publicacion.service';
+import { TokenService } from 'src/app/Services/token.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-modal-editar-publicacion',
@@ -9,37 +12,72 @@ import { PersonaService } from 'src/app/Services/persona.service';
   styleUrls: ['./modal-editar-publicacion.component.css']
 })
 export class ModalEditarPublicacionComponent implements OnInit{
-  persona: Persona[] = [];
+  
+  public publicacion: Publicacion;
+
+  roles: string[];
+  isAdmin = false;
 
   public imagenes: any = [];
   public previsualizacion: string;
+  public imagenBase64: String;
 
   constructor(
-    private personaService: PersonaService,
-    private sanitizer: DomSanitizer
+    private publicacionService: PublicacionService,
+    private sanitizer: DomSanitizer,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private tokenService: TokenService
+
     ) {}
 
   ngOnInit(): void {
 
-    this.mostrarPersona();
+    this.roles = this.tokenService.getAuthorities();
+    this.roles.forEach((rol)=> {
+      if (rol === 'ROLE_ADMIN'){
+        this.isAdmin = true;
+      }
+    })
 
+    const id = this.activatedRoute.snapshot.params['idPublicacion'];
+  this.publicacionService.buscarPublicacion(id).subscribe(
+    data => {
+      this.publicacion = data;
+      this.previsualizacion = this.publicacion.fotoLibro;
+    },
+     err => {
+      alert("Error al cargar la Reseña");
+      this.router.navigate(['/publicaciones']);
+    }
+    )
   }
 
-  private mostrarPersona() {
-    this.personaService.verPersona().subscribe(
-      (data) => {
-        this.persona = data;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+  editar(): void{
+    const id = this.activatedRoute.snapshot.params['idPublicacion'];
+    this.publicacionService.editarPublicacion(id, this.publicacion).subscribe(
+      data => {
+        Swal.fire(
+          'Reseña Editada!',
+          'Se ha editado con exito',
+          'success'
+        )
+        this.router.navigate(['/publicaciones'])
+      }, err => {
+        Swal.fire(
+          'ERROR!',
+          'Ah ocurrido un error al editar la Reseña',
+          'error'
+        )
+        this.router.navigate(['/publicaciones']);
+      })
   }
 
   capturarImagen(event: any){
     const imagenCapturada = event.target.files[0];
     this.extraerBase64(imagenCapturada).then((imagen: any) => {
       this.previsualizacion = imagen.base;
+      this.publicacion.fotoLibro = imagen.base;
     });
   }
 
