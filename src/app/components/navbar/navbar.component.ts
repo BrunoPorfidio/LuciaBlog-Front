1760b330cf3926e0cc9d37c744c95f064f4ed816
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-05-24 20:39:42 -0300

    Agregando Seccion Navbar.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/Services/auth.service'; 
+import { TokenService } from 'src/app/Services/token.service'; 
+import { environments } from 'src/app/environments/environment.prod'; 
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  isLogged = environments.isLogged;
+  isAdmin = false;
+  roles: string[];
+
+  constructor(
+    private auth: AuthService,
+    private tokenService: TokenService
+  ){}
+
+  ngOnInit(): void {
+    this.roles = this.tokenService.getAuthorities();
+    this.roles.forEach((rol) => {
+      if (rol === 'ROLE_ADMIN') {
+        this.isAdmin = true;
+      }
+    });
+
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+   
+  }
+  
+  cerrarsesion(): void {
+    this.tokenService.logOut();
+    window.location.reload();
+  }
 
 }
