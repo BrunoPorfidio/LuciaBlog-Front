701755e11efd53cf19c18a895709113cc34819f8
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-05-24 23:26:39 -0300

    Agregando Seccion Destacados.

--- src/app/components/destacados/destacados.component.ts
+++ src/app/components/destacados/destacados.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Publicacion } from 'src/app/Models/Publicacion';
+import { PublicacionService } from 'src/app/Services/publicacion.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-destacados',
   templateUrl: './destacados.component.html',
   styleUrls: ['./destacados.component.css']
 })
-export class DestacadosComponent {
+export class DestacadosComponent implements OnInit{
+
+  public publicacion: Publicacion;
+
+  constructor(
+    private publicacionService: PublicacionService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.publicacionService.verUltimo().subscribe(
+      (data) => {
+        this.publicacion = data;
+      },
+      (err) => {
+        Swal.fire(
+          'ERROR!',
+          'Error al cargar los Datos',
+          'error'
+        )
+        this.router.navigate(['/luciBlog']);
+      }
+    );
+  }
 
 }
